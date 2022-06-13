b29fc145e7b8b47cf8fcdad402d6097b74d60c86
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-06-13 10:28:24 -0300

    refactor: removendo variaveis do RadioCheck

--- src/components/RadioCheck.tsx
+++ src/components/RadioCheck.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react';
-
 import styles from './RadioCheck.module.scss';
 import checkedBox from "../assets/checked.svg";
 import uncheckedBox from "../assets/unchecked.svg";
-
-
 interface CheckedProps {
   checked?: boolean
 }
 
 export function RadioCheck({checked = false} : CheckedProps) {
-  const [check, setCheck] = useState(checked);  
-
   return (
     <img 
       className={styles.radioCheck} 
