98617aa38cd1cd85a1a7c9ae9c294f0b26861306
Danilo <danilo.calegaro@hotmail.com>
2022-06-13 23:22:01 -0300

    feat: iniciando implementação criar tarefa

--- src/components/InputNewTask.tsx
+++ src/components/InputNewTask.tsx
@@ -1,11 +1,29 @@
 import { PlusCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./InputNewTask.module.scss"
 
 export function InputNewTask() {
+  const [newTaskText, setNewTaskText] = useState('')
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+    console.log('Tarefa criada: ' + newTaskText)
+    setNewTaskText('')
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value)
+  }
+
   return (
     <div className={styles.inputNewTask}>
-      <form>
-        <input type="text" placeholder="Adicione uma nova tarefa"/>
+      <form onSubmit={handleCreateNewTask}>
+        <input 
+          type="text" 
+          placeholder="Adicione uma nova tarefa"
+          value={newTaskText}
+          onChange={handleNewTaskChange}
+        />
         <button type="submit" className={styles.buttonCreate}>
           Criar
           <PlusCircle size={18}/>
